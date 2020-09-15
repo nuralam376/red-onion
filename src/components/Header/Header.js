6830fcdbed64072f5c8482bc1b111f22b19db4af
Nur A Alam <nuraalam376@gmail.com>
2020-09-15 19:46:12 +0600

    designed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Navbar, Nav, Image, Container } from "react-bootstrap";
 
 const Header = () => {
 	return (
-		<div>
-			<h1>Header</h1>
-		</div>
+		<Navbar bg="dark" variant="dark">
+			<Container>
+				<Navbar.Brand href="#home">
+					<Image
+						src="https://i.ibb.co/4NZspmC/logo.png"
+						alt="logo"
+						className="w-25"
+						border="0"
+					/>
+				</Navbar.Brand>
+				<Nav className="ml-auto">
+					<Nav.Link href="#home">Cart</Nav.Link>
+					<Nav.Link href="#features">Login</Nav.Link>
+					<Nav.Link href="#pricing">Signup</Nav.Link>
+				</Nav>
+			</Container>
+		</Navbar>
 	);
 };
 
