a551d74326b8ff908cd9c56dc97fee1a091edf1f
Nur A Alam <nuraalam376@gmail.com>
2020-09-15 20:03:38 +0600

    designed banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
 	return (
-		<div>
-			<h1>Banner</h1>
+		<div className="banner d-flex justify-content-center align-items-center">
+			<div className="">
+				<h1>Best food waiting for your belly</h1>
+				<input type="text" placeholder="Search food items" />
+				<input type="submit" value="Search" />
+			</div>
 		</div>
 	);
 };
